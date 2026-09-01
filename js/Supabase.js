c5bcb2e53bf337e64a4bb1472807b976945224bd
Sanjana Tasmita <317370319+SanjanaTasmita202x@users.noreply.github.com>
2026-09-01 19:58:07 +0600

    Fix index html

--- js/Supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://kzfmjzoksrjazzidkfy.supabase.co";
-
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_5rUtMZvJwkztb8RFAMa0UQ_23wQBNwz";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
